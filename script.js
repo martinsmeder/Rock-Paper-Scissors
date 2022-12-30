b4f301ae0091e4812ca3096efb2010e8d8f59064
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2022-12-30 19:45:56 +0100

    Added case-insensitive player choice function

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ function getComputerChoice() {
     } else if (randomNumber === 2) {
         return "Paper";
     } else {
-        return "Scissor";
+        return "Scissors";
     }
 }
 let computerSelection = getComputerChoice();
 
-// get user choice 
-
+// get case-insensitive player choice 
+function getPlayerChoice() {
+    let playerSelection = prompt("Rock, Paper or Scissors? ").toLowerCase();
+    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+        return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    } else {
+        alert("Wrong input! Type: 'Rock', 'Paper' or 'Scissors' ");
+    }
+}
+let playerSelection = getPlayerChoice();
 
 // play one round 
