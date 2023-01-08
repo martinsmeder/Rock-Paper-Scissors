b9d85303d7cfb23e02fd7bc3d16c864b0b134500
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-01-08 20:58:10 +0100

    Added test.js file

---
+++ test.js
@@ -0,0 +1,16 @@
+const rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click', () => {
+    alert("Rock!");
+});
+
+
+const paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', () => {
+    alert("Paper!");
+});
+
+const scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener('click', () => {
+    alert("Scissors!")
+});
+
